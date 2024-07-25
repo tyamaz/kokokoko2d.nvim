a7fafb803c3426ab0e192e6f54ad24f49933961e
tyamaz <pokkuri@gmail.com>
2024-07-25 13:02:17 +0700

    change render algorythm

--- lua/kokokoko2d/cursor_sign_render.lua
+++ lua/kokokoko2d/cursor_sign_render.lua
@@ -35,7 +35,7 @@ end
 --- @param buffer any    対象のバッファ
 --- @param character any どの文字を描画するか 1文字
 --- @param hlgroup any   対象の文字に対する hlgroup
---- @param pos any       位置 { lnum = 1, cnum = 10 } みたいな
+--- @param pos any       位置 { lnum = 1, cnum = 10 } みたいな 1 開始換算
 --- @param speed any     表示している時間(ミリ秒)、この時間後勝手に消去される
 --- @param priority any  優先度 デカい値が優先に表示される
 function M.render_one(buffer, character, hlgroup, pos, speed, priority)
@@ -48,11 +48,11 @@ function M.render_one(buffer, character, hlgroup, pos, speed, priority)
   local extmark_id = vim.api.nvim_buf_set_extmark(
       buffer,
       ns_id,
-      pos.lnum - 1,  -- ここで与えるパラメータはゼロスタート
+      pos.lnum - 1,  -- 行指定 この関数は 0 開始換算なので 1 引く
       0,   -- 横位置調整は virt_text_pos_win_col で行うのでゼロ指定
       {
         virt_text_pos = "overlay",         -- 既存の上に載せて出す
-        virt_text_win_col = pos.cnum - 1,  -- 位置指定
+        virt_text_win_col = pos.cnum - 1,  -- 事実上列位置指定 この関数は 0 開始換算なので 1 引く
         virt_text = {{character, hlgroup}},  -- 何出すか
         priority = priority
       })
@@ -88,8 +88,8 @@ function M.render(buffer, pos, speed)
   end
 end
 
-
---- 始点と終点を指定してその間を描画する
+--- 【no use】
+--- 始点と終点を指定してその間を描画する 行ベース
 --- @param buffer any
 --- @param from_pos any
 --- @param to_pos any
@@ -153,6 +153,200 @@ function M.render_range(buffer, from_pos, to_pos, speed)
     ::continue::
   end
 
+end
+
+--- 【no use】
+--- 始点と終点を指定してその間を描画する 列ベース
+--- 列の移動をベースに軌跡を描画する
+--- 行の移動に対して列の移動が多い場合に使うとよいかも
+--- 上下に長い距離を移動する場合はこちらのほうが見た目が良好
+--- @param buffer any
+--- @param from_pos any
+--- @param to_pos any
+--- @param speed any
+function M.render_range_based_column(buffer, from_pos, to_pos, speed)
+  -- 行移動ベースで差分を蓄積することで位置を求める
+  -- 折りたたみを無視する
+  local dist_l = count_line_without_folded(buffer, from_pos.lnum, to_pos.lnum)
+
+  -- 同一行ならしない この場合は同一行でも行う
+
+  local dist_c = math.abs(to_pos.cnum - from_pos.cnum)
+
+  if dist_c < 1 then
+    -- 差無しでも動作させるために1を入れておく
+    dist_c = 1
+  end
+  -- 1列あたり何行分移動させればよいか(小数)
+  local l_step = dist_l / dist_c
+
+  -- 1列に何秒間描画すればよいか
+  -- 内部ではこの時間を元に軌跡が全部繋がるように時間を決める
+  local column_speed = math.floor(speed / dist_c)   -- 距離は列ベースとする
+  if column_speed == 0 then
+    column_speed = 1
+  end
+  -- どっちに進むか
+  local updown = from_pos.lnum > to_pos.lnum and -1 or 1
+  local lr = from_pos.cnum > to_pos.cnum and -1 or 1
+
+  -- 誤差を少なくするための厳密な行位置の計算
+  -- 整数計算だけでやる方法よりも分かりやすさ重視
+  local pos_l = from_pos.lnum
+
+  local count_folded = 0
+
+  -- 行ベースでループ
+  for c = from_pos.cnum, to_pos.cnum, lr do
+    -- 最後は描画しない
+    if c == to_pos.cnum then
+       goto continue
+    end
+    local l = 0
+    if updown == -1 then
+      -- 上方向に向かっているなら切り下げる(行きすぎないように下(大きい数字)に戻す)
+      l = math.ceil(pos_l)
+    elseif updown == 1 then
+      -- 下方向に向かっているなら切り上げる(行きすぎないように上(小さい数字)に戻す)
+      l = math.floor(pos_l)
+    end
+    -- 行が折りたたまれて表示されていないなら描画しない
+    if is_folded(buffer, l) then
+      count_folded = count_folded + 1
+      goto continue
+    end
+
+    local order = math.abs(c - from_pos.cnum) - count_folded  -- 折りたたみを飛ばしている分実際はもっと近い
+
+    timer_util.setTimeout(
+        function()
+          M.render(buffer, {lnum = l, cnum = c}, speed)
+        end,
+        column_speed * order)
+    -- ↑ 終点にに接近するほど描画するまでが遅い
+    -- つまり、始点から終点に進んでいるように見える
+
+    -- 厳密に位置を積算する
+    -- c_step に折りたたみが無い分が織り込み済み
+    pos_l = pos_l + (updown * l_step)
+    ::continue::
+  end
+end
+
+
+--- 始点と終点を指定してその間を描画する 縦横距離ベース
+--- 縦横の距離ベースに軌跡を描画する。飛ばされる部分無くビッチリ描画される
+--- @param buffer any
+--- @param from_pos any {lnum = 12, cnum = 34}  みたいな。行列共に 1 スタート換算
+--- @param to_pos any
+--- @param speed any
+function M.render_range_based_dist(buffer, from_pos, to_pos, speed)
+  -- 行移動ベースで差分を蓄積することで位置を求める
+  -- 折りたたみを無視する
+  local dist_l = count_line_without_folded(buffer, from_pos.lnum, to_pos.lnum)
+  local dist_c = math.abs(to_pos.cnum - from_pos.cnum)
+
+
+  -- 1行当たりの1列の描画量(小数)
+  -- Lua はゼロ除算でエラーでなく結果無限に設定される
+  local step_c = 0
+  if dist_l < 1 then
+    step_c = 300.0  -- 1行描画するには十分な量
+  else
+    step_c = dist_c / dist_l
+  end
+
+  if dist_c < 1 then
+    -- 差無しでも動作させるために1を入れておく
+    dist_c = 1
+  end
+
+  -- 1エリアに何秒間描画すればよいか
+  -- 内部ではこの時間を元に軌跡が全部繋がるように時間を決める
+  local render_speed = math.floor(speed / (dist_l + (dist_c / 2)))
+  if render_speed < 1 then
+    render_speed  = 1
+  end
+  -- どっちに進むか
+  local updown = 0  -- 進まないならゼロ
+  if from_pos.lnum > to_pos.lnum then
+    updown = -1  -- from がでかいから下から上に向かう
+  elseif from_pos.lnum < to_pos.lnum then
+    updown = 1   -- from が小さいから上から下に向かう
+  end
+  local lr = 0  -- 進まないならゼロ
+  if from_pos.cnum > to_pos.cnum then
+    lr = -1 -- from がデカいから右から左に向かう
+  elseif  from_pos.cnum < to_pos.cnum then
+    lr = 1  -- from が小さいから左から右に向かう
+  end
+
+
+  -- 誤差を少なくするための厳密な列描画位置の計算
+  -- 整数計算だけでやる方法よりも分かりやすさ重視
+  local pos_c = from_pos.cnum
+
+  -- 折れている行数カウント(描画しない)
+  local count_folded = 0
+  -- 何個描画したかカウント
+  local count_render = 0
+
+
+
+  -- 行ベースでループ 基本行 → 列 で描画していく
+  -- 進まない場合が1ループだけする updown 0 だと無限ループになるので適当な値を入れて1周だけするようにする
+  for l = from_pos.lnum, to_pos.lnum, (updown == 0 and 100 or updown) do
+    -- 行が折りたたまれて表示されていないなら描画しない
+    if is_folded(buffer, l) then
+      count_folded = count_folded + 1
+      goto continue
+    end
+
+    -- 複数行あるやつの最後の1行だけ動作させない
+    -- つまり単独1周は処理する
+    if (from_pos.lnum ~= to_pos.lnum) and (l == to_pos.lnum) then
+      goto continue
+    end
+
+    local from_pos_c = 0
+    local to_pos_c = 0
+    if lr == -1 then
+      -- 左方向に向かっているなら切り上げる(行きすぎないように右(大きい数字)に戻す)
+      from_pos_c = math.ceil(pos_c)
+      to_pos_c = math.ceil(pos_c + (lr * step_c))
+    elseif lr == 1 then
+      -- 右方向に向かっているなら切り下げる(行きすぎないように左(小さい数字)に戻す)
+      from_pos_c = math.floor(pos_c)
+      to_pos_c = math.floor(pos_c + (lr * step_c))
+    elseif lr == 0 then
+      -- 動かない場合はその場1周のみ
+      from_pos_c = math.floor(pos_c)
+      to_pos_c = math.floor(pos_c)
+    end
+
+    -- 列ベースでループ
+    -- そもそも横方向は狭いし、マーカーは広いし、描画も早いので間引く
+    -- 動かない場合でも1周だけ描画はする
+    for c = from_pos_c, to_pos_c, (lr == 0 and 100 or lr) do
+      -- 1周のみでない且つ奇数行のみ描画する
+      if (from_pos_c ~= to_pos_c) and (c % 2 == 1) then
+        -- goto skiprender
+      end
+      timer_util.setTimeout(
+          function()
+            M.render(buffer, {lnum = l, cnum = c}, speed)
+          end,
+          render_speed * count_render)
+      -- ↑ 終点にに接近するほど描画するまでが遅い
+      -- つまり、始点から終点に進んでいるように見える
+
+      ::skiprender::
+      count_render = count_render + 1
+    end
+    -- 厳密に位置を積算する
+    pos_c = pos_c + (lr * step_c)
+    ::continue::
+  end
 end
 
 
--- lua/kokokoko2d/init.lua
+++ lua/kokokoko2d/init.lua
@@ -1,11 +1,30 @@
 local render = require("kokokoko2d.cursor_sign_render")
 local M = {}
-M.version = "0.0.1"
+M.version = "0.0.2"
+
+
+
+--- マルチバイト考慮した本当のカーソル位置のカラム位置
+--- 1 文字目を 1 と換算する
+local function get_visual_corrent_col()
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local line = cursor_pos[1]
+    local col = cursor_pos[2]
+    -- 対象行全行をもってきて
+    local line_str = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+    -- 行頭からカーソル列までの幅をマルチバイト込みで測る
+    return vim.fn.strdisplaywidth(line_str:sub(1, col)) + 1
+end
+
+
+
 
 --- 初期化セット
+--- called is enable
 function M.setup()
   -- 残像に使うマーカー的文字の管理と初期化
   require("kokokoko2d.sign_define").setup()
+
 
   vim.api.nvim_create_augroup('kokokoko2d', { clear = true })
 
@@ -14,9 +33,9 @@ function M.setup()
     group = 'kokokoko2d',
     callback = function()
       -- plugin 関係のバッファを無視する
-     if vim.bo.filetype == 'lazy' then
+      if vim.bo.filetype == 'lazy' then
         return
-     end
+      end
 
       -- 戻った瞬間の位置を1個前の位置として記録する
       vim.api.nvim_buf_set_var(0, 'kokokoko2d_prev_lnum', vim.fn.line('.'))
@@ -35,15 +54,19 @@ function M.setup()
             return
          end
         local cb = vim.api.nvim_get_current_buf()
+
+        -- 行列共に1スタート換算
         local prev_lnum = vim.api.nvim_buf_get_var(0, 'kokokoko2d_prev_lnum')
         local prev_cnum = vim.api.nvim_buf_get_var(0, 'kokokoko2d_prev_cnum')
+
         local curt_lnum = vim.fn.line('.')
-        local curt_cnum = vim.fn.col('.')
+        local curt_cnum = get_visual_corrent_col() -- マルチバイト文字対応で、文字混在でも正しいカラム位置を出す
+
         local top_lnum = vim.fn.line('w0')
         local bottom_lnum = vim.fn.line('w$')
         local from_lnum = prev_lnum
 
-        local speed = 300
+        local speed = 200
 
         if prev_lnum < top_lnum then
           from_lnum = top_lnum
@@ -53,7 +76,7 @@ function M.setup()
           from_lnum = bottom_lnum
         end
         -- 始点と終点の間に軌跡のサインを描画する
-        render.render_range(
+        render.render_range_based_dist(
           cb,
           {
             lnum = from_lnum,
@@ -61,7 +84,7 @@ function M.setup()
           },
           {lnum = curt_lnum, cnum = curt_cnum},
           speed)
-
+-- ほげほげほげhごえほえgほえgほえgほえhごえhごえhごえhごえhごえhごえhごえhごえhごへごえhごえごえhごえhごほえgへおhhhhhhhhhhhh
         -- 1個前の位置を現在地点で更新
         vim.api.nvim_buf_set_var(0, 'kokokoko2d_prev_lnum', curt_lnum)
         vim.api.nvim_buf_set_var(0, 'kokokoko2d_prev_cnum', curt_cnum)
